4ab5b29d1f8db0aef9eca22a3ed4bfa8d48c75c2
elias pölönen <elias.polonen06@outlook.com>
2025-01-10 13:16:28 +0200

    1.2

--- pagespa/src/Main.js
+++ pagespa/src/Main.js
@@ -1,30 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import { Routes, Route, NavLink, HashRouter } from "react-router-dom";
 import Koti from "./Koti";
 import Tarina from "./Tarina";
 import Yhteys from "./Yhteys";
- 
-class Main extends Component {
-    render() {
-      return (
-        <HashRouter>
-          <div>
-            <h1>Perus Single Page Application (SPA)</h1>
-            <ul className="header">
-              <li><NavLink exact to="/">Koti</NavLink></li>
-              <li><NavLink to="/tarina">Tarinaa</NavLink></li>
-              <li><NavLink to="/yhteys">Yhteys</NavLink></li>
-            </ul>
-            <div className="content">
-                <Routes>
-                    <Route path="/" element={<Koti />}/>
-                    <Route path="/tarina" element={<Tarina />}/>
-                    <Route path="/yhteys" element={<Yhteys />}/>
-                </Routes>
-            </div>
-          </div>
-        </HashRouter>
-      );
-    }
-  }
+
+const Main = () => {
+  return (
+    <HashRouter>
+      <div>
+        <h1>Perus Single Page Application (SPA)</h1>
+        <ul className="header">
+          <li><NavLink exact="true" to="/">Koti</NavLink></li>
+          <li><NavLink to="/tarina">Tarinaa</NavLink></li>
+          <li><NavLink to="/yhteys">Yhteys</NavLink></li>
+        </ul>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Koti />} />
+            <Route path="/tarina" element={<Tarina />} />
+            <Route path="/yhteys" element={<Yhteys />} />
+          </Routes>
+        </div>
+      </div>
+    </HashRouter>
+  );
+};
+
 export default Main;
